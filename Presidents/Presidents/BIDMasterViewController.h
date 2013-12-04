83f83705f20e286c5201a51502f1fded1a9f3177
Jovi Nel <jnp.parilla@gmail.com>
2013-12-04 13:58:55 +0800

    First commit, initialize data for the project
    Signed-off-by: Jovi Nel <jnp.parilla@gmail.com>

---
+++ Presidents/Presidents/BIDMasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  BIDMasterViewController.h
+//  Presidents
+//
+//  Created by Maria Alice C. Lim on 12/4/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BIDDetailViewController;
+
+@interface BIDMasterViewController : UITableViewController
+
+@property (strong, nonatomic) BIDDetailViewController *detailViewController;
+
+@end
